7ba4c7f979edf5a498fcf3bcba0efbb63fa2f1de
Lorenzo Sonnino <sonnino.lorenzo@icloud.com>
2021-07-09 10:46:09 +0200

    Fixed pathfinding

--- client/Battleground/Assets/Scripts/Core/Gameplay/Field.cs
+++ client/Battleground/Assets/Scripts/Core/Gameplay/Field.cs
@@ -6,13 +6,22 @@ using UnityEngine.Tilemaps;
 public class Field
 {
     private int width, height;
+    private int offsetX, offsetY;
     private bool[,] walkable;
 
     private PathFind.Grid pathFind;
 
     public Field(Tilemap[] nonCollidingTilemaps, Tilemap[] collidingTilemaps) {
+        // Get offset and size
         for (int i=0 ; i < nonCollidingTilemaps.Length ; i++) {
+            Vector3 origin = nonCollidingTilemaps[i].origin;
             Vector3 size = nonCollidingTilemaps[i].cellBounds.size;
+            if (origin.x < offsetX) {
+                offsetX = (int) origin.x;
+            }
+            if (origin.y < offsetY) {
+                offsetY = (int) origin.y;
+            }
             if (size.x > width) {
                 width = (int) size.x;
             }
@@ -21,7 +30,14 @@ public class Field
             }
         }
         for (int i=0 ; i < collidingTilemaps.Length ; i++) {
+            Vector3 origin = collidingTilemaps[i].origin;
             Vector3 size = collidingTilemaps[i].cellBounds.size;
+            if (origin.x < offsetX) {
+                offsetX = (int) origin.x;
+            }
+            if (origin.y < offsetY) {
+                offsetY = (int) origin.y;
+            }
             if (size.x > width) {
                 width = (int) size.x;
             }
@@ -30,82 +46,62 @@ public class Field
             }
         }
 
+        // Set walkable
         walkable = new bool[width, height];
-        for (int x=0 ; x < width ; x++) {
-            for(int y=0 ; y < height ; y++) {
-                Vector3Int pos = new Vector3Int(x, y, 0);
-                bool found = false;
-
-                // Check if there is a collision
-                for (int i=0 ; i < collidingTilemaps.Length ; i++) {
-                    Vector3 size = collidingTilemaps[i].cellBounds.size;
-                    if (x < size.x && y < size.y && collidingTilemaps[i].HasTile(pos)) {
-                        walkable[x, y] = false;
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (found) { continue; }
-
-                // Check if there is a walkable tile
-                for (int i=0 ; i < nonCollidingTilemaps.Length ; i++) {
-                    Vector3 size = nonCollidingTilemaps[i].cellBounds.size;
-                    if (x < size.x && y < size.y && nonCollidingTilemaps[i].HasTile(pos)) {
-                        walkable[x, y] = true;
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (found) { continue; }
-
-                // There is no tile there
-                walkable[x, y] = false;
-            }
+        for (int i=0 ; i < nonCollidingTilemaps.Length ; i++) {
+            AddCollisions(nonCollidingTilemaps[i], false);
+        }
+        for (int i=0 ; i < nonCollidingTilemaps.Length ; i++) {
+            AddCollisions(collidingTilemaps[i], true);
         }
 
+        // Set pathfinder instance
         pathFind = new PathFind.Grid(walkable);
     }
 
+    public bool IsInBounds(int x, int y) {
+        return !(x - offsetX < 0 || y - offsetY < 0 ||
+            x - offsetX >= width || y - offsetY >= height);
+    }
     /*
      * Get the path from one point to another. The returning list is empty
      * if there is none
      */
     public List<PathFind.Point> GetPath(int fromX, int fromY, int toX, int toY) {
-        if (fromX < 0 || fromX > width || fromY < 0 || fromY > height ||
-            toX < 0 || toX > width || toY < 0 || toY > height) {
+        if (!IsInBounds(fromX, fromY) || !IsInBounds(toX, toY)) {
             return null;
         }
 
-        PathFind.Point _from = new PathFind.Point(fromX, fromY);
-        PathFind.Point _to = new PathFind.Point(toX, toY);
+        PathFind.Point _from = new PathFind.Point(fromX - offsetX, fromY - offsetY);
+        PathFind.Point _to = new PathFind.Point(toX - offsetX, toY - offsetY);
 
         return PathFind.Pathfinding.FindPath(pathFind, _from, _to, PathFind.Pathfinding.DistanceType.Manhattan);
     }
 
     /*
-     * Update the pathfinding to take new collisions into account
-     * Takes a tilemap with collisions enabled smaller or of the same size
-     * as the largest tilemap taken into account when instantiating this object
+     * Update walkable by taking into account a new Tilemap
+     * NOTE: non collision tilemaps must be all taken into account before the collision
+     * ones are added. Otherwise a non-collision tilemap might override a collision
      */
-    public void AddCollisions(Tilemap newMap) {
+    private void AddCollisions(Tilemap newMap, bool collision) {
+        Vector3 origin = newMap.origin;
         Vector3 size = newMap.cellBounds.size;
-        bool updated = false;
 
         for (int x=0 ; x < width ; x++) {
             for(int y=0 ; y < height ; y++) {
-                if (x < size.x && y < size.y && newMap.HasTile(new Vector3Int(x, y, 0))) {
-                    // If the place was walkable, it is not anymore: update the
-                    // grid at the end of the function
-                    updated |= walkable[x, y];
+                if (offsetX + x < origin.x || offsetY + y < origin.y ||
+                    offsetX + x >= origin.x + size.x || offsetY + y >= origin.y + size.y) {
+                    continue;
+                }
+
+                bool hasTile = newMap.HasTile(new Vector3Int(offsetX + x, offsetY + y, 0));
+                if (collision && hasTile) {
                     walkable[x, y] = false;
                 }
+                else if (!walkable[x, y] && !collision){
+                    walkable[x, y] = true;
+                }
             }
-        }
-
-        if (updated) {
-            pathFind.UpdateGrid(walkable);
         }
     }
 }
